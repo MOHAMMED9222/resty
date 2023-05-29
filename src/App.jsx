17eb165eba1c8efcf551b8253cbf8ede93af6f22
Mohammed Abubakar <mohammedfahim363@yahoo.com>
2023-05-29 10:50:46 -0700

    reduce

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
 
 import './App.scss';
@@ -8,45 +8,105 @@ import './App.scss';
 // OFFICIALLY, we have chosen to use the Airbnb style guide naming convention. 
 // Why is this source of truth beneficial when spread across a global organization?
 import Header from './Components/Header';
+
+import Results from './Components/Results';
+import History from './Components/History';
 import Footer from './Components/Footer';
 import Form from './Components/Form';
-import Results from './Components/Results';
+
+export const initialState = {
+  data: null,
+  requestParams: {},
+  loading: false,
+  history: []
+}
+
+export const reducer = (state=initialState, action) => {
+  switch(action.type){
+    case 'START-REQUEST':
+      return {
+        ...state,
+        loading: true,
+        requestParams: action.payload
+      };
+    case 'FINISH-REQUEST':
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+        history: [...state.history, 
+          {
+            requestParams: {...state.requestParams}, 
+            data: action.payload
+          }
+        ],
+      }
+      case 'CHANGE-HISTORY':
+        return {
+          ...state,
+          ...state.history[action.payload],
+        }
+
+    default:
+      return state;
+  }
+};
 
 function App() {
 
-  const [data, setData] = useState('');
-  const [requestParams, setRequestParams] = useState({})
-  const [loading, setLoading] = useState(false);
+  // const [data, setData] = useState('');
+  // const [requestParams, setRequestParams] = useState({})
+  // const [loading, setLoading] = useState(false);
 
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const callApi = (requestParams) => {
-    setLoading(true);
-    setTimeout(() => {
-      setRequestParams(requestParams);
-      setLoading(false);
-    }, 1000);
+
+      let action = {
+        type: 'START-REQUEST',
+        payload: requestParams
+      }
+      dispatch(action);
+ 
+  }
+
+  const changeHistory = (idx) => {
+    let action = {
+      type: 'CHANGE-HISTORY',
+      payload: idx,
+    }
+    dispatch(action)
   }
 
   useEffect(() => {
 
     const getData = async () => {
-      console.log('TESTING BEFORE AXIOS ---- >', requestParams)
-      let response = await axios(requestParams)
-      console.log('RESPONSE------->', response.data)
-      setData(response.data.results);
+      if (state.requestParams.method && state.requestParams.url){
+      // console.log('TESTING BEFORE AXIOS ---- >', requestParams)
+      let response = await axios(state.requestParams)
+      const data = response.data;
+      let action = {
+        type: 'FINISH-REQUEST',
+        payload: data,
+      }
+      dispatch(action);
+      // console.log('RESPONSE------->', response.data)
+      // setData(response.data.results);
+
+      }
     }
     getData();
-  }, [requestParams]);
-
+  }, [state.requestParams]);
 
   return (
     <React.Fragment>
       <Header />
-      <div>Request Method: {requestParams.method}</div>
-      <div>URL: {requestParams.url}</div>
-      <div>Sent data: {requestParams.textArea} </div>
+      <div>Request Method: {state.requestParams.method}</div>
+      <div>URL: {state.requestParams.url}</div>
+      <div>Sent data: {state.requestParams.textArea} </div>
       <Form handleApiCall={callApi} />
-      <Results data={data} loading={loading} />
+      <History history={state.history} changeHistory={changeHistory} />
+      <Results data={state.data} loading={state.loading} />
       <Footer />
     </React.Fragment>
   );
---
+++ src/Components/History/index.jsx
@@ -0,0 +1,25 @@
+import './history.scss'
+
+const History = (props) => {
+ 
+ const { history, changeHistory } = props;
+
+  return(
+    <>
+      <ul>
+        { history.length ?
+          history.map((record, idx) => (
+            <li key={`history-${idx}`}>
+              <button onClick={() => changeHistory(idx)}>
+                {record.requestParams.method}: {record.requestParams.url}
+              </button>
+              </li>
+          )) : ''
+        }
+      </ul>
+    </>
+  )
+
+};
+
+export default History;
